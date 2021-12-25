fa9a20275e15b0894c9f9b22c36c0397e29ead30
Щербич Данил <denilya2@yandex.ru>
2021-12-25 18:43:11 +0500

    Добавил футер, поменял css

--- reacttestim/testim/src/App.js
+++ reacttestim/testim/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Header =() => {
+  return (
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Хочу спать
+    </p>
+  </header>
+  )
+}
+
+const Main = () => {
+  return (
+    <main>
+      <h2>
+        Стоит ли становиться программистом, если всю жизнь менеджеришь проекты?
+      </h2>
+    </main>
+  )
+}
+const Footer = () =>
+{
+  return (
+    <footer>
+      <h3>
+        Когда удалил лол, что бы научиться кодить на реакте и заработать денег на скин. Ps это шутка...не удалил
+      </h3>
+    </footer>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Хочу спать
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
